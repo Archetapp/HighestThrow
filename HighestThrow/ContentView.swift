4ecd0f7698ae23f2a3a1d5b5da383e52f7a505c2
Jared Davidson (Archetapp) <archetapp@gmail.com>
2023-05-15 09:51:31 +0530

    Initial Commit

---
+++ HighestThrow/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HighestThrow
+//
+//  Created by Jared Davidson on 5/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HighestThrow/HighestThrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighestThrowApp.swift
+//  HighestThrow
+//
+//  Created by Jared Davidson on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct HighestThrowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
